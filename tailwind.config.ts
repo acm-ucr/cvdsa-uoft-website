66adb3130545b506883d00a3b68c8bca06ae255a
minh <minhhdtran@gmail.com>
2024-12-17 14:32:59 -0800

    add colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        cvdsa: {
+          red: "#F33735",
+          "red-100": "#F2918F",
+          orange: "#FF754A",
+          purple: "#BD99FF",
+          cornflower: "#B5CBFD",
+        },
+      },
+    },
   },
   plugins: [],
 };
