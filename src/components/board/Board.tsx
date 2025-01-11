00e3d04a1bf3bff1b33df3770cf485cea2359a5e
Stanley Lew <stanley43595@gmail.com>
2025-01-10 17:44:03 -0800

    format code

--- src/components/board/Board.tsx
+++ src/components/board/Board.tsx
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Board = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  return <div></div>;
+};
 
-export default Board
+export default Board;
