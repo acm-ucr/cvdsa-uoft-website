2c58ef68fd54d5bce7f76689f04f487af70aa6f5
minh <minhhdtran@gmail.com>
2025-01-10 17:59:38 -0800

    test

---
+++ src/components/home/example.tsx
@@ -0,0 +1,9 @@
+const example = () => {
+  return (
+    <div className="flex items-center justify-center text-[10vw] text-cvdsa-red-200">
+      example
+    </div>
+  );
+};
+
+export default example;
