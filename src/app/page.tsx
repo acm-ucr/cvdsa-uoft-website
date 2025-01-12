101fb3e04d345bf3c0d9afb149733afa4da69f8e
Aiden Wu <wu@Aidens-MacBook-Air.local>
2025-01-11 16:26:18 -0800

    add JoinText

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Example from "@/components/home/example";
+import JoinText from "@/components/home/JoinText";
 
 const Home = () => {
   return (
     <div>
       <Example />
+      <JoinText />
     </div>
   );
 };
---
+++ src/components/home/JoinText.tsx
@@ -0,0 +1,9 @@
+const JoinText = () => {
+  return (
+    <div className="flex w-full justify-center">
+      <p className="w-[70%] text-[4vw]">WHY JOIN?</p>
+    </div>
+  );
+};
+
+export default JoinText;
