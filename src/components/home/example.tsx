5014202c10a845e57050b0d369395de46e703093
Andrew Chan <ac_mac@ucr-secure-28-10-13-162-185.wnet.ucr.edu>
2025-01-11 18:38:19 -0800

    added join now button

--- src/components/home/example.tsx
+++ src/components/home/example.tsx
@@ -1,8 +1,15 @@
 const example = () => {
   return (
-    <div className="flex items-center justify-center text-[10vw] text-cvdsa-red-200">
-      example
-    </div>
+    <>
+      <div className="flex items-center justify-center text-[10vw] text-cvdsa-red-200">
+        example
+      </div>
+      <div className="flex items-center justify-center">
+        <button className="mt-56 rounded-full border px-6 py-2 text-3xl">
+          JOIN NOW!
+        </button>
+      </div>
+    </>
   );
 };
 
