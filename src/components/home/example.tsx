b9741998131e12cee18d0f1a0db67cdc5b83fe79
JSimjee <jameelsimjee@gmail.com>
2025-01-15 08:19:25 -0800

    just making some practice changes

--- src/components/home/example.tsx
+++ src/components/home/example.tsx
@@ -1,7 +1,12 @@
 const example = () => {
   return (
-    <div className="flex items-center justify-center text-[10vw] text-cvdsa-red-200">
-      example
+    <div className="items-center justify-center text-[4vw] text-cvdsa-red-200">
+      University of the Atlantic Ocean
+
+      <div className="flex items-right justify-right text-[2vw] text-cvdsa-blue-100">
+      University of the Atlantic Ocean
+      </div>
+
     </div>
   );
 };
