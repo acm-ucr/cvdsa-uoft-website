183dc2ccbf8d806067c13e3af3d7b6de4bb0b36e
JSimjee <jameelsimjee@gmail.com>
2025-01-16 10:34:31 -0800

    added custom font with 500 weight at usage time. Still need to have correct BG and text color, and the font looks slightly different than the figma

--- src/components/home/example.tsx
+++ src/components/home/example.tsx
@@ -1,6 +1,12 @@
 const example = () => {
   return (
-    <div className="flex h-screen w-screen items-center text-center mx-auto justify-center text-[4vw] text-cvdsa-white-255">
+    <div className="
+    text-5x1/48.76
+    font-cvdsa-Montserrat-sans
+    font-[500]
+    flex h-screen w-screen items-center text-center mx-auto justify-center 
+    text-[4vw] 
+    text-cvdsa-white-255">
       CARDIOVASCULAR DISEASE STUDENT ASSOCIATION
     </div>
   );
