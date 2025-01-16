505000c8534d4e7e89e5fd20e68353d9789c7153
Phoebe Zhou <phoebezhou@Phoebes-Air.freshman.emma.troy.ny.us>
2025-01-15 21:36:00 -0500

    phoebe_research

---
+++ src/components/home/research.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Research = () => {
+    return (
+    <div className="flex items-center justify-center font-seasons text-[3vw]">
+        <p>RESEARCH</p>
+      </div>
+    );
+  };
+  
+  export default Research;
