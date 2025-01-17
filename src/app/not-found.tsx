867bc8d53d2318a1d68c4092dfba1a7d817ea2bf
Shrish Vishnu Rajesh Kumar <shrishvishnu06@gmail.com>
2025-01-16 22:43:53 -0800

    Implemented Sorry, the page.. text to 404 error page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,14 @@
 const NotFound = () => {
-  return <div>404 Page Not Found</div>;
+  return (
+    <div className="mx-auto grid h-screen grid-cols-1 content-center">
+      <div className="flex justify-center">
+        <p className="font-monster text-4xl font-normal">
+          Sorry, the page you were<br></br>
+          looking for was not found!
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
