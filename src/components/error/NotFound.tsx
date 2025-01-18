39e9c612c003d0c748b602a7f6eefbfa6de6bc49
Andrew Trang <andrewtrang@Andrews-MacBook-Air-57.local>
2025-01-18 17:52:47 -0500

    fix bold

--- src/components/error/NotFound.tsx
+++ src/components/error/NotFound.tsx
@@ -4,7 +4,7 @@ const HomeButton = () => {
     <div className="flex h-screen items-center justify-center">
       <Link
         href="/"
-        className="rounded-full border bg-gradient-to-r from-cvdsa-orange-100 to-cvdsa-purple-100 px-6 py-2 text-2xl font-bold text-white shadow-lg"
+        className="rounded-full border bg-gradient-to-r from-cvdsa-orange-100 to-cvdsa-purple-100 px-6 py-2 text-2xl font-medium text-white shadow-lg"
       >
         Go Back Home
       </Link>
