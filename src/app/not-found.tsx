d35c0fc0956d770ef3e6bee2abbfcafde729232d
Shrish Vishnu Rajesh Kumar <shrishvishnu06@gmail.com>
2025-01-17 18:16:36 -0800

    removed grid and used default font setting

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,10 @@
 const NotFound = () => {
   return (
-    <div className="mx-auto grid h-screen grid-cols-1 content-center">
-      <div className="flex justify-center">
-        <p className="font-monster text-4xl font-normal">
-          Sorry, the page you were<br></br>
-          looking for was not found!
-        </p>
-      </div>
+    <div className="flex h-screen items-center justify-center">
+      <p className="text-4xl font-normal">
+        Sorry, the page you were<br></br>
+        looking for was not found!
+      </p>
     </div>
   );
 };
