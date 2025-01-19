c5fe7ede0eba1a2fb09ceec00a60c23e3be3c971
ACM UCR <fake@domain.com>
2025-01-19 13:16:39 +0000

    convert images to webp

--- src/components/home/aboutUsBlurb.tsx
+++ src/components/home/aboutUsBlurb.tsx
@@ -1,8 +1,8 @@
 const AboutUsBlurb = () => {
   return (
     <div className="relative mt-[30vh] flex items-center justify-center">
-      <img src="home/aboutusframe.png" width={1200} className="flex-shrink-0" />
-      <img src="home/aboutusimg.png" width={580} className="absolute left-48" />
+      <img src="home/aboutusframe.webp" width={1200} className="flex-shrink-0" />
+      <img src="home/aboutusimg.webp" width={580} className="absolute left-48" />
     </div>
   );
 };
