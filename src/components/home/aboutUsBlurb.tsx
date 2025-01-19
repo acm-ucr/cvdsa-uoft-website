d1fd606c3ec97a7d65dcab76746e0c938b759f78
Andrew Chan <ac_mac@Andrews-Air-2.home>
2025-01-19 05:10:25 -0800

    added about us blurb

---
+++ src/components/home/aboutUsBlurb.tsx
@@ -0,0 +1,10 @@
+const AboutUsBlurb = () => {
+  return (
+    <div className="relative mt-[30vh] flex items-center justify-center">
+      <img src="home/aboutusframe.png" width={1200} className="flex-shrink-0" />
+      <img src="home/aboutusimg.png" width={580} className="absolute left-48" />
+    </div>
+  );
+};
+
+export default AboutUsBlurb;
