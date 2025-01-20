a60cefd14b9c8cd7b9f23415bfb1fb5ee8684707
Phoebe Zhou <phoebezhou@Phoebes-Air.freshman.emma.troy.ny.us>
2025-01-19 22:43:22 -0500

    phoebe_research_page_banner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import MainText from "@/components/home/mainText";
 import Research from "@/components/research/Research";
+import ResearchImage from "@/components/research/page";
 
 const Home = () => {
   return (
     <div>
       <MainText />
       <Research />
+      <ResearchImage />
     </div>
   );
 };
