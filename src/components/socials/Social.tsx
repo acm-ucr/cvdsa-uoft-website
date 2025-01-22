241871a367f8418bff5f2c07fa723667f0090c2c
Vivian Tang <ziyuetang@Vivians-Air.freshman.emma.troy.ny.us>
2025-01-21 19:07:44 -0500

    vivian_socials_banner

--- src/components/socials/Social.tsx
+++ src/components/socials/Social.tsx
@@ -2,11 +2,10 @@ import React from "react";
 
 const Social = () => {
   return (
-    <div className="relative flex h-[236px] w-full overflow-hidden">
+    <div className="relative flex w-full overflow-hidden">
       <img
         src="/social/socialsimage.png"
         alt="Socials Banner"
-        className="h-full w-full object-cover object-[center_45%]"
       />
     </div>
   );
