cd4afc49a6012219254316d7a304fafca0bd0a0f
ACM UCR <fake@domain.com>
2025-01-22 00:09:07 +0000

    convert images to webp

--- src/components/socials/Social.tsx
+++ src/components/socials/Social.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Social = () => {
   return (
     <div className="relative flex w-full overflow-hidden">
-      <img src="/social/socialsimage.png" alt="Socials Banner" />
+      <img src="/social/socialsimage.webp" alt="Socials Banner" />
     </div>
   );
 };
