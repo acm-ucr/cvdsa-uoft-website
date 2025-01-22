7347c13ad86b2202d79fd059806712ddfdd4bbbf
JSimjee <jameelsimjee@gmail.com>
2025-01-22 13:58:08 -0800

    applied requested changes

--- src/components/home/example.tsx
+++ src/components/home/example.tsx
@@ -1,12 +1,8 @@
 const example = () => {
   return (
     <div className="
-    text-5x1/48.76
-    font-cvdsa-Montserrat-sans
-    font-[500]
-    flex h-screen w-screen items-center text-center mx-auto justify-center 
-    text-[4vw] 
-    text-cvdsa-white-255">
+    flex h-screen w-screen items-center 
+    text-center mx-auto justify-center text-[4vw] text-black">
       CARDIOVASCULAR DISEASE STUDENT ASSOCIATION
     </div>
   );
