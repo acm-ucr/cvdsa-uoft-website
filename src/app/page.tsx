92dcec3a4ad7a39210d036a8e0ed904a1f393753
JSimjee <jameelsimjee@gmail.com>
2025-01-23 07:58:54 -0800

    experimenting with new classes, learning where they are called (in src/app/page.tsx) and where they are declared and defined (src/components/[whatever you want]/[your component type].tsx)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import MainText from "@/components/home/mainText";
+import Events from "../components/events/Events";
 
 const Home = () => {
   return (
     <div>
       <MainText />
+      <Events/>
     </div>
   );
 };
