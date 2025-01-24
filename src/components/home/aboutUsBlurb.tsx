f5194cd415f59e01528f50948b012d786f6d3be9
Andrew Chan <ac_mac@ucr-secure-28-10-13-160-181.wnet.ucr.edu>
2025-01-24 14:53:00 -0800

    about us blurb updated text

--- src/components/home/aboutUsBlurb.tsx
+++ src/components/home/aboutUsBlurb.tsx
@@ -1,24 +1,24 @@
-const AboutUsBlurb = () => {
-  return (
-    <div className="ml-[10vw] mt-[5vw] h-[40vw] w-[80vw] rounded-3xl border-4">
-      <div className="mt-[39vh] flex items-center justify-center">
-        <img
-          src="home/aboutusimg.webp"
-          className="absolute mr-[35vw] w-[39vw]"
-        />
+  const AboutUsBlurb = () => {
+    return (
+      <div className="ml-[10vw] mt-[5vw] h-[40vw] w-[80vw] rounded-3xl border-4">
+        <div className="mt-[39vh] flex items-center justify-center">
+          <img
+            src="home/aboutusimg.webp"
+            className="absolute mr-[35vw] w-[39vw]"
+          />
+        </div>
+        <h2 className="ml-[45vw] mt-[-15vw] font-seasons text-6xl"> ABOUT US </h2>
+        <h3 className="ml-[45vw] mt-[2vw] max-w-[30vw] font-seasons text-[20px]">
+          The Cardiovascular Disease Student Association (CVDSA) is a student-led
+          advocacy group dedicated to raising awareness and funds in collaboration
+          with charities like the Heart & Stroke Foundation of Canada. Our
+          organization operates through five distinct committees: Events and
+          Education, Research, Finance, Marketing, and Develpment. Together, we
+          aim to increase awareness of cardiovascular diseases, inspire student
+          engagement in research, and support charitable fundraising efforts.
+        </h3>
       </div>
-      <h2 className="ml-[45vw] mt-[-15vw] font-seasons text-6xl"> ABOUT US </h2>
-      <h3 className="ml-[45vw] mt-[2vw] max-w-[30vw] font-seasons text-[20px]">
-        The Cardiovascular Disease Student Association (CVDSA) is a student-led
-        advocacy group dedicated to raising awareness and funds in collaboration
-        with charities like the Heart & Stroke Foundation of Canada. Our
-        organization operates through five distinct committees: Events and
-        Education, Research, Finance, Marketing, and Develpment. Together, we
-        aim to increase awareness of cardiovascular diseases, inspire student
-        engagement in research, and support charitable fundraising efforts.
-      </h3>
-    </div>
-  );
-};
+    );
+  };
 
-export default AboutUsBlurb;
+  export default AboutUsBlurb;
