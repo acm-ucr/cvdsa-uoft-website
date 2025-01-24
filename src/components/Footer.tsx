07b5b03769d0adcb5af00de946c133869da59915
Andrew Trang <andrewtrang@Andrews-MacBook-Air-57.local>
2025-01-24 02:04:10 -0500

    footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <div className="flex w-full bg-gradient-to-r from-cvdsa-red-200 to-cvdsa-purple-100">
+      <div>
+        <p>Subscribe to our Newsletter!</p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
