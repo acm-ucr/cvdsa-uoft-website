52b26012692fe1e6259c93091e50af6218bd4c2d
Andrew Chan <achan541@ucr.edu>
2025-01-24 14:08:01 -0800

    Update src/components/home/aboutUsBlurb.tsx
    Co-authored-by: minh  <148649841+minhhdtran@users.noreply.github.com>

--- src/components/home/aboutUsBlurb.tsx
+++ src/components/home/aboutUsBlurb.tsx
@@ -2,7 +2,7 @@ const AboutUsBlurb = () => {
   return (
     <div className="relative mt-[30vh] flex items-center justify-center">
       <img src="home/aboutusframe.webp" width={1200} className="flex-shrink-0" />
-      <img src="home/aboutusimg.webp" width={580} className="absolute left-48" />
+      <img src="home/aboutusimg.webp" className="absolute left-48 w-[___vw]" />
     </div>
   );
 };
