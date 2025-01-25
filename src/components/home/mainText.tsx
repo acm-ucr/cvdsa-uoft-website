ecab0f61846eb9af51618ca16a0ce62cc4dd1763
Shrish Vishnu Rajesh Kumar <shrishvishnu06@gmail.com>
2025-01-25 13:57:58 -0800

    added background image and made text white

--- src/components/home/mainText.tsx
+++ src/components/home/mainText.tsx
@@ -2,10 +2,13 @@ import React from "react";
 
 const MainText = () => {
   return (
-    <div className="flex justify-center font-seasons text-[6vw]">
-      <p>UNIVERSITY OF TORONTO</p>
+    <div className="flex h-[1200px] w-full justify-center bg-[url('/home/landing.svg')] bg-cover bg-center bg-no-repeat font-seasons text-[6vw]">
+      <p className="text-white">UNIVERSITY OF TORONTO</p>
     </div>
   );
 };
 
 export default MainText;
+
+//w-full aspect-[16/9]
+//h-auto w-full
