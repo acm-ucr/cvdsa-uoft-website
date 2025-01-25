bfb815cfc5eef5f1729e459ca88bbea5dd6041cf
JSimjee <jameelsimjee@gmail.com>
2025-01-25 03:02:54 -0800

    not sure how to format page.tsx, will check dev branch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import MainText from "@/components/home/mainText";
-import JoinText from "@/components/home/JoinText";
 import BannerImage from "@/components/events/BannerImage";
 
 const Home = () => {
