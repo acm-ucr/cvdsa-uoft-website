9e0b417539d5c65f02dff63ee1a6a1c400269dc6
JSimjee <jameelsimjee@gmail.com>
2025-01-25 03:03:42 -0800

    MainText and BannerImage are in page.tsx, idk what else there needs to be

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import MainText from "@/components/home/mainText";
 import BannerImage from "@/components/events/BannerImage";
 
 const Home = () => {
   return (
     <div>
+      <MainText />
       <BannerImage />
     </div>
   );
