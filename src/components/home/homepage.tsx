897bef37f14e95b3ee8120b1428681b790c0b99b
JSimjee <jameelsimjee@gmail.com>
2025-01-28 12:09:45 -0800

    made secondary text match figma

--- src/components/home/homepage.tsx
+++ src/components/home/homepage.tsx
@@ -1,6 +1,6 @@
 const HomePage = () => {
   return (
-    <div className="mx-auto flex h-screen w-screen items-center justify-center text-center text-[4vw] text-black">
+    <div className="mx-auto flex w-screen items-center justify-center text-center text-[4vw] text-black">
       CARDIOVASCULAR DISEASE STUDENT ASSOCIATION
     </div>
   );
