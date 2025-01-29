aba0678819f117c83fe29c2d67d28e2bb469585e
Vivian Tang <ziyuetang@Vivians-Air.freshman.emma.troy.ny.us>
2025-01-28 23:29:33 -0500

    changed the format

---
+++ src/components/socials/Social.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import socialsimage from "@/public/social/socialsimage.webp";
+
+const Social = () => {
+  return (
+    <div className="relative flex w-full overflow-hidden">
+      <Image src={socialsimage} alt="Socials Banner" />
+    </div>
+  );
+};
+
+export default Social;
