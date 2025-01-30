3212567f895da471f7946ea71560add316600081
Phoebe Zhou <phoebezhou@Phoebes-Air.freshman.emma.troy.ny.us>
2025-01-30 09:26:33 -0500

    phoebe_prop_update

--- src/components/home/BlurbHome.tsx
+++ src/components/home/BlurbHome.tsx
@@ -1,14 +1,16 @@
 import Image from "next/image";
 import Cupcakes from "@/public/home/cupcakes.webp";
-interface CardProps {
-  title: string;
-  description: string;
-  image: string;
-}
+import Props from "@/components/home/BlurbHomeProps";
+import Grid from "@/components/home/grid";
 
-const BlurbHome: React.FC<CardProps> = ({ title, description, image }) => {
+const BlurbHome = () => {
   return (
     <>
+      <Grid />
+      <Props title="hi" description="hey" image="" />
+      <Props title="hey" description="me" image="" />
+      <Props title="hello" description="hey" image="" />
+
       <div className="flex w-[30vw] flex-col items-center">
         <div>
           {image}
@@ -18,19 +20,18 @@ const BlurbHome: React.FC<CardProps> = ({ title, description, image }) => {
             className="h-[37vh] w-[20vw] rounded-[15%] border-2 border-cvdsa-cornflower-100"
           />
         </div>
-      </div>
-      <h2 className="my-[1vh] text-[1.7vw] font-medium">
-        {title}Collaborations
-      </h2>
 
-      <p className="w-[18vw] text-center text-[0.9vw] font-light leading-tight">
-        {description}
-        We collaborate with various organizations, including SAGE and St. John
-        Ambulance (SJA) Canada. We plan to host campus-wide events and are
-        actively networking with other groups focused on cardiovascular disease
-        initiatives!
-      </p>
+        <p className="my-[1vh] text-[1.7vw] font-medium">Collaborations</p>
+
+        <p className="w-[18vw] text-center text-[0.9vw] font-light leading-tight">
+          We collaborate with various organizations, including SAGE and St. John
+          Ambulance (SJA) Canada. We plan to host campus-wide events and are
+          actively networking with other groups focused on cardiovascular
+          disease initiatives!
+        </p>
+      </div>
     </>
   );
 };
+
 export default BlurbHome;
