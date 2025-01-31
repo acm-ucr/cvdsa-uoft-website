437433bcc21c9042a660e3095ccdf9b8003f58e3
Andrew Chan <achan541@ucr.edu>
2025-01-31 15:07:47 -0800

    Update src/app/page.tsx
    Co-authored-by: Stanley Lew <135681592+stanleylew5@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import BlurbHome from "@/components/home/BlurbHome";
 import MainText from "@/components/home/mainText";
-import Research from "@/components/research/Research";
 import JoinButton from "@/components/home/joinbutton";
 import AboutUsBlurb from "@/components/home/aboutUsBlurb";
 import HomePage from "@/components/home/homepage";
