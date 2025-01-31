f99d699500ff62a3c45bd7c496eda20a699c2d71
Asir Mazhar <76020138+asirmazhar23@users.noreply.github.com>
2025-01-31 04:46:34 -0800

    Update src/components/research/secondText.tsx
    Co-authored-by: Stanley Lew <135681592+stanleylew5@users.noreply.github.com>

--- src/components/research/secondText.tsx
+++ src/components/research/secondText.tsx
@@ -1,6 +1,6 @@
 const SecondaryText = () => {
   return (
-    <p className="align-center flex justify-center text-center font-seasons text-xl">
+    <p className="flex flex-col justify-center text-center font-seasons text-xl">
       Click on these for more information
     </p>
   );
