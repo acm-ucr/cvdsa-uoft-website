6bd1f863d93ae4a7fb713820e8bc4011153908c5
Shrish Vishnu Rajesh Kumar <shrishvishnu06@gmail.com>
2025-01-31 16:50:53 -0800

    used image tag instead, added z-indexes since text was covered by absolutely positioned image

--- src/components/home/mainText.tsx
+++ src/components/home/mainText.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import Image from "next/image";
+import landing from "@/public/home/landing.svg";
 
 const MainText = () => {
   return (
-    <div className="flex aspect-[1414/1000] w-full justify-center bg-[url('/home/landing.svg')] bg-cover bg-center bg-no-repeat font-seasons text-[6vw]">
-      <p className="text-white">UNIVERSITY OF TORONTO</p>
+    <div className="flex justify-center font-seasons text-[6vw]">
+      <Image
+        src={landing}
+        alt="landing"
+        className="absolute z-0 w-screen"
+      ></Image>
+      <p className="z-10 text-white">UNIVERSITY OF TORONTO</p>
     </div>
   );
 };
@@ -12,3 +19,5 @@ export default MainText;
 
 //w-full aspect-[16/9]
 //h-auto w-full
+
+//flex aspect-[1414/1000] w-full justify-center bg-[url('/home/landing.svg')] bg-cover bg-center bg-no-repeat font-seasons text-[6vw]
