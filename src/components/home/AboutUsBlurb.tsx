f0b00528d6c7610d725b3f88f16ce9fb57d70249
Stanley Lew <stanley43595@gmail.com>
2025-02-02 02:19:55 -0800

    cleanup

--- src/components/home/AboutUsBlurb.tsx
+++ src/components/home/AboutUsBlurb.tsx
@@ -3,17 +3,11 @@ import aboutBlurb from "@/public/home/aboutusimg.webp";
 
 const AboutUsBlurb = () => {
   return (
-    <div className="relative mx-[10vw] flex h-[40vw] justify-center rounded-3xl border-4 px-[3vw] py-[4vh]">
-      <div>
-        <Image
-          src={aboutBlurb}
-          alt="Group Club Photo"
-          className="mr-[5vw] w-[40vw]"
-        />
-      </div>
-      <div className="mt-[2vh] flex-col">
-        <p className="font-seasons text-[9vh]"> ABOUT US </p>
-        <p className="max-w-[30vw] font-seasons text-[2.8vh]">
+    <div className="mx-[10vw] flex justify-center rounded-3xl border-4 px-[3vw] py-[4vh]">
+      <Image src={aboutBlurb} alt="Group Club Photo" className="mr-[5vw]" />
+      <div className="flex-col">
+        <p className="font-seasons text-[9vh]">ABOUT US</p>
+        <p className="font-seasons text-[2.8vh]">
           The Cardiovascular Disease Student Association (CVDSA) is a
           student-led advocacy group dedicated to raising awareness and funds in
           collaboration with charities like the Heart & Stroke Foundation of
