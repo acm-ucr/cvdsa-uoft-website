abb3d64461680d2fa03a70e63d48ebaa52ac8b39
JSimjee <jameelsimjee@gmail.com>
2025-02-06 16:26:42 -0800

    formatting

--- src/app/events/page.tsx
+++ src/app/events/page.tsx
@@ -1,9 +1,11 @@
 import BannerImage from "@/components/events/BannerImage";
 
 const Events = () => {
-  return <div>
-  <BannerImage />;
-  </div >;
+  return (
+    <div>
+      <BannerImage />;
+    </div>
+  );
 };
 
 export default Events;
