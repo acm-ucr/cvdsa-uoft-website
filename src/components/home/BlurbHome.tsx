ab63f8dfedeb5124efcfec75d9754b4772c410fd
Andrew Chan <achan541@ucr.edu>
2025-02-07 17:33:47 -0800

    fixed spacing

--- src/components/home/BlurbHome.tsx
+++ src/components/home/BlurbHome.tsx
@@ -5,7 +5,7 @@ const BlurbHome = () => {
   return (
     <div className="mt-[23vh]">
       <p className="ml-[10vw] font-seasons text-[4vw]">WHY JOIN?</p>
-      <div className="mx-[12vw] mt-[11vh] columns-3 grid-rows-1">
+      <div className="mx-[6vw] mt-[11vh] columns-3 grid-rows-1">
         {blurbData.map((blurb, index) => (
           <BlurbHomeProps
             key={index}
