383094bfc9e01e1f4c3e267b0abfa89e89a839a6
Asir Mazhar <asirmazhar23@gmail.com>
2025-02-08 10:44:38 -0800

    secondaryTextResearch

---
+++ src/components/research/secondaryText.tsx
@@ -0,0 +1,11 @@
+const SecondaryText = () => {
+  return (
+    <p className="flex justify-center text-center font-seasons md:text-xl">
+      Click on these for access to more
+      <br />
+      resources!
+    </p>
+  );
+};
+
+export default SecondaryText;
