2601ab734e4d80742a7a59819fa4903b5d12bfa4
JSimjee <jameelsimjee@gmail.com>
2025-02-12 21:32:55 -0800

    it's always the darn formatting that I forget

--- src/components/board/MemberCard.tsx
+++ src/components/board/MemberCard.tsx
@@ -1,15 +1,11 @@
 import "lucide-react";
 
-const MemberCard = ({
-  name
-}: {
-  name: string;
-}) => {
+const MemberCard = ({ name }: { name: string }) => {
   return (
     <div className="flex w-[21vw] flex-col items-center">
-      <p className="mb-[5%] text-[2vw] font-medium">{name}</p>  
+      <p className="mb-[5%] text-[2vw] font-medium">{name}</p>
     </div>
-  )
+  );
 };
 
 export default MemberCard;
