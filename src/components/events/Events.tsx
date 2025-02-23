3bf1456695abdc88776bb152e650b2e2bd8561ec
Phoebe Zhou <phoebezhou@Phoebes-Air.freshman.emma.troy.ny.us>
2025-02-23 15:50:17 -0500

    phoebe/upcoming_events

--- src/components/events/Events.tsx
+++ src/components/events/Events.tsx
@@ -1,23 +1,6 @@
 import React from "react";
 import EventsProps from "./EventsProps";
 
-const events = [
-  {
-    date: "20",
-    month: "OCT",
-    title: "NAME",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  },
-  {
-    date: "21",
-    month: "OCT",
-    title: "NAME",
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-  },
-];
-
 const UpcomingEvents = () => {
   return (
     <div className="mt-8 flex flex-col items-center">
@@ -25,9 +8,18 @@ const UpcomingEvents = () => {
         UPCOMING EVENTS
       </h2>
       <div className="flex gap-12">
-        {events.map((event, index) => (
-          <EventsProps key={index} {...event} />
-        ))}
+        <EventsProps
+          date="20"
+          month="OCT"
+          title="NAME"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
+        <EventsProps
+          date="21"
+          month="OCT"
+          title="NAME"
+          description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+        />
       </div>
     </div>
   );
