be7232b7959cc32352b7a6b5e85adc9e3a830e89
stanleylew5 <stanley43595@gmail.com>
2025-02-25 15:12:32 -0800

    small change

--- src/components/events/EventCard.tsx
+++ src/components/events/EventCard.tsx
@@ -1,29 +1,26 @@
-import React from "react";
-
-interface CardProps {
+interface EventCardProps {
   date: string;
   month: string;
   title: string;
   description: string;
 }
-const EventsProps: React.FC<CardProps> = ({
+const EventCard: React.FC<EventCardProps> = ({
   date,
   month,
   title,
   description,
 }) => {
   return (
-    <div className="flex max-w-xl items-center overflow-hidden rounded-lg shadow-lg">
-      <div className="ml-8 mr-8 flex flex-col items-center rounded-l-lg bg-white p-3 text-center">
+    <div className="grid grid-cols-4 items-center rounded-lg shadow-lg">
+      <div className="flex flex-col items-center rounded-l-lg bg-white text-center">
         <p className="font-seasons text-[2vw]">{month}</p>
         <p className="text-[4vw]">{date}</p>
       </div>
-
-      <div className="rounded-r-lg bg-indigo-200 py-4 pl-6">
+      <div className="col-span-3 rounded-r-lg bg-indigo-200 py-4 pl-6">
         <p className="text-[2vw]">{title}</p>
         <p className="text-[1vw] text-gray-700">{description}</p>
       </div>
     </div>
   );
 };
-export default EventsProps;
+export default EventCard;
--- src/components/events/Events.tsx
+++ src/components/events/Events.tsx
@@ -1,20 +1,19 @@
-import React from "react";
-import EventsProps from "./EventsProps";
+import EventCard from "./EventCard";
 
 const UpcomingEvents = () => {
   return (
-    <div className="mt-8 flex flex-col items-center">
-      <h2 className="m-4 flex justify-center font-seasons text-[4vw]">
+    <div className="flex flex-col items-center">
+      <p className="m-4 flex justify-center font-seasons text-[4vw]">
         UPCOMING EVENTS
-      </h2>
-      <div className="flex gap-12">
-        <EventsProps
+      </p>
+      <div className="flex w-10/12 gap-4">
+        <EventCard
           date="20"
           month="OCT"
           title="NAME"
           description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
         />
-        <EventsProps
+        <EventCard
           date="21"
           month="OCT"
           title="NAME"
