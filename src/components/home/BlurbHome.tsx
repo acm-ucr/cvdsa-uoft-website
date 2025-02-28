ad745f7b4ad3a6d8995adc490f924e6b70f05287
JSimjee <jameelsimjee@gmail.com>
2025-02-28 00:59:24 -0800

    forgot to paste back components I removed, I'm gonna put them as separate components later

--- src/components/home/BlurbHome.tsx
+++ src/components/home/BlurbHome.tsx
@@ -1,15 +1,16 @@
 "use client";
 import BlurbHomeProps from "./BlurbHomeProps";
 import { blurbCollaborations } from "@/data/blurbData.js";
-import {motion} from "motion/react";
+import { motion } from "motion/react";
 
 const BlurbHome = () => {
   return (
     <div className="mt-[23vh]">
       <p className="ml-[10vw] font-seasons text-[4vw]">WHY JOIN?</p>
-      <motion.div className="mx-[6vw] mt-[11vh] columns-3 grid-rows-1"
-      whileInView={{opacity: ["20%", "100%"], x: ["20%", "0%"]}}
-      transition={{duration:1}}
+      <motion.div
+        className="mx-[6vw] mt-[11vh] columns-3 grid-rows-1"
+        whileInView={{ opacity: ["20%", "100%"], x: ["20%", "0%"] }}
+        transition={{ duration: 1 }}
       >
         {blurbCollaborations.map((blurb, index) => (
           <BlurbHomeProps
