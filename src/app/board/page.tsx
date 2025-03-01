6b1c0839226dd48a2a55dcfd326fd2cbd772d4ee
Phoebe Zhou <phoebezhou@Phoebes-Air.freshman.emma.troy.ny.us>
2025-03-01 00:31:58 -0500

    phoebe_committee_file_data_and_implementation

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,9 +1,11 @@
 import MemberCards from "@/components/board/MemberCards";
+import CommitteeCards from "@/components/board/CommitteeCards";
 
 const Board = () => {
   return (
     <div>
       <MemberCards />
+      <CommitteeCards />
     </div>
   );
 };
--- src/components/board/MemberCards.tsx
+++ src/components/board/MemberCards.tsx
@@ -1,25 +1,11 @@
-import React from "react";
-import { MdOutlineEmail } from "react-icons/md";
-import { resComInfo } from "./MemberCard"; // Import stored team data
+import MemberCard from "./MemberCard";
 
 const MemberCards = () => {
   return (
-    <div className="p-12">
-      <div className="m-3 mb-6 flex items-center font-seasons text-[2vw]">
-        <p>Research Committee</p>
+    <div className="flex w-screen justify-center">
+      <div className="grid grid-cols-3 gap-[10vw]">
+        <MemberCard name={"James Buckwas (fake name)"} />
       </div>
-
-      {resComInfo.map((member, email) => (
-        <div
-          key={email}
-          className="m-3 mb-2 flex items-center gap-2 text-[3vw]"
-        >
-          <a href={"malito:{member.email}"} className="hover text-red-500">
-            <MdOutlineEmail size={20} />
-          </a>
-          <p className="text-[1.5vw] text-black">{member.name}</p>
-        </div>
-      ))}
     </div>
   );
 };
