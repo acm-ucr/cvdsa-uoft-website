990e992fa94d3d52e1225293b22cb30e15c98a3a
stanleylew5 <stanley43595@gmail.com>
2025-03-02 03:36:20 -0800

    move file into data folder

--- src/app/board/page.tsx
+++ src/app/board/page.tsx
@@ -1,9 +1,11 @@
 import MemberCards from "@/components/board/MemberCards";
+import ResearchCommittee from "@/components/board/ResearchCommittee";
 
 const Board = () => {
   return (
     <div>
       <MemberCards />
+      <ResearchCommittee />
     </div>
   );
 };
