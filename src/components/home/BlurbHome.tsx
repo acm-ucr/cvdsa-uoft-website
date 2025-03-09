08b3be5d2564cac0f8928792af5fda36f420a32c
stanleylew5 <stanley43595@gmail.com>
2025-03-09 00:54:41 -0800

    all landing animations done

--- src/components/home/BlurbHome.tsx
+++ src/components/home/BlurbHome.tsx
@@ -1,11 +1,38 @@
+"use client";
 import BlurbHomeProps from "./BlurbHomeProps";
 import blurbData from "@/data/blurbData";
+import * as motion from "motion/react-client";
+
+const Down = {
+  initial: { opacity: 0, y: -20 },
+  animate: (custom: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.8, delay: custom },
+  }),
+};
 
 const BlurbHome = () => {
   return (
     <div className="mt-[7vh] md:mt-[15vh]">
-      <p className="ml-[10vw] font-seasons text-5xl md:text-7xl">WHY JOIN?</p>
-      <div className="mx-[6vw] mt-[11vh] grid grid-cols-1 md:grid-cols-3">
+      <motion.p
+        variants={Down}
+        viewport={{ once: true, amount: 0 }}
+        initial="initial"
+        whileInView={"animate"}
+        custom={0.4}
+        className="ml-[10vw] font-seasons text-5xl md:text-7xl"
+      >
+        WHY JOIN?
+      </motion.p>
+      <motion.div
+        variants={Down}
+        viewport={{ once: true, amount: 0 }}
+        initial="initial"
+        whileInView={"animate"}
+        custom={0.9}
+        className="mx-[6vw] mt-[11vh] grid grid-cols-1 md:grid-cols-3"
+      >
         {blurbData.map(({ title, description, image, alt }, index) => (
           <BlurbHomeProps
             key={index}
@@ -15,7 +42,7 @@ const BlurbHome = () => {
             alt={alt}
           />
         ))}
-      </div>
+      </motion.div>
     </div>
   );
 };
