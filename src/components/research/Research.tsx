2d1e50166d0da37645232fd3ec81344fe5c3ee43
stanleylew5 <stanley43595@gmail.com>
2025-03-22 02:10:38 -0700

    done

--- src/components/research/Research.tsx
+++ src/components/research/Research.tsx
@@ -1,5 +1,20 @@
+"use client";
 import Image from "next/image";
 import ResearchConference from "@/public/research/ResearchConference.webp";
+import ResearchCard from "./ResearchCard";
+import articles from "@/public/research/articles.png";
+import podcasts from "@/public/research/podcasts.png";
+import * as motion from "motion/react-client";
+
+const Down = {
+  initial: { opacity: 0, y: -15 },
+  animate: (custom: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: { duration: 1, delay: custom, ease: "easeOut" },
+  }),
+};
+
 const Research = () => {
   return (
     <div className="flex flex-col justify-center text-center text-[3vw]">
@@ -8,9 +23,55 @@ const Research = () => {
         className="w-screen"
         alt={"Research Conference"}
       />
-      <p className="font-seasons">RESEARCH</p>
-      <p className="text-xl">Click on these for access to more</p>
-      <p className="text-xl">resources!</p>
+      <motion.p
+        variants={Down}
+        viewport={{ once: true }}
+        initial="initial"
+        whileInView={"animate"}
+        custom={0}
+        className="pt-6 font-seasons"
+      >
+        RESEARCH
+      </motion.p>
+      <motion.p
+        variants={Down}
+        viewport={{ once: true }}
+        initial="initial"
+        whileInView={"animate"}
+        custom={0.4}
+        className="text-xl font-semibold"
+      >
+        Click on these for access to more
+      </motion.p>
+      <motion.p
+        variants={Down}
+        viewport={{ once: true }}
+        initial="initial"
+        whileInView={"animate"}
+        custom={0.4}
+        className="text-xl font-semibold"
+      >
+        resources!
+      </motion.p>
+      <motion.div
+        variants={Down}
+        viewport={{ once: true }}
+        initial="initial"
+        whileInView={"animate"}
+        custom={0.8}
+        className="mx-[20%] grid grid-cols-1 pb-8 pt-4 md:grid-cols-2"
+      >
+        <ResearchCard
+          name="Articles"
+          photo={articles}
+          link="https://drive.google.com/file/d/1QHbk4PIiay8uEqw0iwcJmhFjp_ZyKdsX"
+        />
+        <ResearchCard
+          name="Podcasts"
+          photo={podcasts}
+          link="https://open.spotify.com/show/14BLo7ajpvhOWuBxsv3lrl?si=b2c4543a9d8f4f54"
+        />
+      </motion.div>
     </div>
   );
 };
